0c4ddfa71e4bae9cd56f7ad45839344b6ad0d084
mkoczy1 <93876962+mkoczy1@users.noreply.github.com>
2024-08-07 22:28:58 +0200

    Update sql_prevent.java

--- sql_prevent.java
+++ sql_prevent.java
@@ -1,21 +1,26 @@
+import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class SQLInjectionPrevention {
 
-    public static void main(String[] args) {
-        try {
-            // Verbindungsdetails zur Datenbank
-            String url = "jdbc:mysql://localhost:3306/demo";
-            String username = "root";
-            String password = "password";
-            
-            // Verbindung zur Datenbank herstellen
-            Connection connection = DriverManager.getConnection(url, username, password);
+    private static DataSource dataSource;
 
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/demo");
+        config.setUsername("root");
+        config.setPassword("password"); // Sollte aus einem sicheren Speicherort geladen werden
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = dataSource.getConnection()) {
             // Benutzereingaben lesen
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter your username: ");
@@ -23,18 +28,18 @@ public class SQLInjectionPrevention {
             System.out.print("Enter your password: ");
             String userPassword = scanner.nextLine();
             
-            // **Sichere SQL-Abfrage mit Prepared Statement**
+            // Sichere SQL-Abfrage mit Prepared Statement
             String query = "SELECT * FROM users WHERE username = ? AND password = ?";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, userInput);
-            preparedStatement.setString(2, userPassword);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                System.out.println("User: " + resultSet.getString("username"));
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, userInput);
+                preparedStatement.setString(2, userPassword);
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        System.out.println("User: " + resultSet.getString("username"));
+                    }
+                }
             }
-
             scanner.close();
-            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
